3603dcdb8e7ad77c9953a77894d586fee5c53dac
maliha <malihanawshin@gmail.com>
2018-10-17 22:02:21 +0600

    created canvas to draw digit, all 3 options ready to take input

--- app/src/main/java/com/example/imm/anko/DrawDigitActivity.java
+++ app/src/main/java/com/example/imm/anko/DrawDigitActivity.java
@@ -1,13 +1,65 @@
 package com.example.imm.anko;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class DrawDigitActivity extends AppCompatActivity {
+
+    protected DrawView drawview;
+    protected ImageView canvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_in_canvas);
+
+        drawview = findViewById(R.id.drawView);
+        canvas = findViewById(R.id.canvasImageview);
+
+        findViewById(R.id.recognizeButton).setOnClickListener(clickListener);
+        findViewById(R.id.resetButton).setOnClickListener(clickListener);
     }
+
+
+    protected View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+                case R.id.recognizeButton:
+                     onRecognize();
+                     break;
+                case R.id.resetButton:
+                     onReset();
+                     break;
+            }
+        }
+    };
+
+    private void onRecognize() {
+
+        Bitmap original = drawview.getBitmap();
+        Bitmap scaled = Bitmap.createScaledBitmap(original, 32, 32, false);
+        canvas.setImageBitmap(scaled);
+        switchPreviewVisibility();
+
+
+    }
+
+    private void switchPreviewVisibility() {
+        canvas.setVisibility(View.VISIBLE);
+        drawview.setVisibility(View.GONE);
+    }
+
+
+    private void onReset() {
+        drawview.reset();
+        canvas.setVisibility(View.GONE);
+        drawview.setVisibility(View.VISIBLE);
+    }
+
 }
---
+++ app/src/main/java/com/example/imm/anko/DrawView.java
@@ -0,0 +1,123 @@
+package com.example.imm.anko;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.View;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+public class DrawView extends View {
+
+    private Path path;
+    private Paint bitmapOfPaint;
+    private Paint paint;
+    private Bitmap bitmap;
+    private Canvas canvas;
+
+    private float X, Y;
+    private static final float TOUCH_TOLERANCE = 5;
+
+    public DrawView(Context context, AttributeSet set) {
+        super(context,set);
+        bitmapOfPaint = new Paint(Paint.DITHER_FLAG);
+        path = new Path();
+    }
+
+    private void setup() {
+        int width = getMeasuredWidth();
+
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeCap(Paint.Cap.SQUARE);
+        paint.setStrokeJoin(Paint.Join.BEVEL);
+        paint.setStrokeWidth(width/32);
+
+        this.paint = paint;
+    }
+
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+        bitmap = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+        setup();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawBitmap(bitmap, 0, 0, bitmapOfPaint);
+        canvas.drawPath(path, paint);
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    private void touchStart(float x, float y) {
+        path.reset();
+        path.moveTo(x, y);
+        X = x;
+        Y = y;
+    }
+
+    private void touchMove(float x, float y) {
+        float fx = Math.abs(x - X);
+        float fy = Math.abs(y - Y);
+        if (fx >= TOUCH_TOLERANCE || fy >= TOUCH_TOLERANCE) {
+            path.quadTo(X, Y, (x + X) / 2, (y + Y) / 2);
+            X = x;
+            Y = y;
+        }
+    }
+
+    private void touchUp() {
+        path.lineTo(X, Y);
+        canvas.drawPath(path, paint);
+        path.reset();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touchUp();
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+    public void reset() {
+        int height = getMeasuredHeight();
+        int width = getMeasuredWidth();
+        onSizeChanged(width,height,width,height);
+        invalidate();
+    }
+}
